b86765b9921561e6980b9aa65f3d1648d6f867a6
Semen Sokolov <s.sokolov@wirenboard.ru>
2021-08-19 15:42:03 +0300

    mounted on -> link

--- wb/mqtt_metrics/metrics.py
+++ wb/mqtt_metrics/metrics.py
@@ -24,10 +24,14 @@ def get_df(cluster):
     p = subprocess.Popen('df -m | grep {0}'.format(cluster), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     cmd_res = p.stdout.readlines()[0].decode()
     df_def_root_data = cmd_res.split()
-    mounted_on = df_def_root_data[5]
     used = df_def_root_data[2]
     total = df_def_root_data[1]
-    return [mounted_on, used, total]
+    return [used, total]
+
+
+def get_dev_root_link():
+    p = subprocess.Popen("mount|grep ' / '|cut -d' ' -f 1", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return p.stdout.readlines()[0].decode()
 
 
 class LoadAverage(Metric):
@@ -59,19 +63,19 @@ class DevRoot(Metric):
         device_messenger.create('dev_root_total_space', 'value', 'MiB')
         device_messenger.create('dev_root_mounted_on', 'text')
         df_dev_root = get_df('/dev/root')
-        device_messenger.send('dev_root_mounted_on', df_dev_root[0])
-        device_messenger.send('dev_root_total_space', df_dev_root[2])
+        device_messenger.send('dev_root_mounted_on', get_dev_root_link())
+        device_messenger.send('dev_root_total_space', df_dev_root[1])
 
     def send(self, device_messenger):
-        device_messenger.send('dev_root_used_space', get_df('/dev/root')[1])
+        device_messenger.send('dev_root_used_space', get_df('/dev/root')[0])
 
 
 class Data(Metric):
     def create(self, device_messenger):
         device_messenger.create('data_used_space', 'value', 'MiB')
         device_messenger.create('data_total_space', 'value', 'MiB')
-        device_messenger.send('data_total_space', get_df('/dev/mmcblk0p6')[2])
+        device_messenger.send('data_total_space', get_df('/dev/mmcblk0p6')[1])
 
     def send(self, device_messenger):
-        device_messenger.send('data_used_space', get_df('/dev/mmcblk0p6')[1])
+        device_messenger.send('data_used_space', get_df('/dev/mmcblk0p6')[0])
 
