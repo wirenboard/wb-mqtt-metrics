79c2ecb3a3a8d3311305988a62255d222a33236a
Maslov Nikita <webconn@users.noreply.github.com>
2022-12-07 21:20:28 +0600

    fix rising CPU usage (#5)
    * code format
    * make pylint happier
    * try to optimize resource usage
     * fix possible Popen resource leak
     * do not use shell
    * add CODEOWNERS

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from setuptools import setup
 
-setup(name='wb-mqtt-metrics',
-      version='0.2.0',
-      description='metrics sender',
-      author='Sokolov Semen',
-      author_email='s.sokolov@wirenboard.ru',
-      packages=['wb.mqtt_metrics']
-      )
+setup(
+    name="wb-mqtt-metrics",
+    version="0.2.0",
+    description="metrics sender",
+    author="Sokolov Semen",
+    author_email="s.sokolov@wirenboard.ru",
+    packages=["wb.mqtt_metrics"],
+)
--- wb/mqtt_metrics/metrics_sender.py
+++ wb/mqtt_metrics/metrics_sender.py
@@ -1,45 +1,52 @@
-import random
-import time
-from threading import Thread
-from .metrics_dict import METRICS
-from .device_messenger import DeviceMessenger
-import sys
-import yaml
-from yaml.loader import SafeLoader
 import argparse
+import random
+import sys
+import time
 import urllib.parse
+from threading import Thread
 
-from paho.mqtt import client as mqtt_client
 import paho_socket
+import yaml
+from paho.mqtt import client as mqtt_client
+from yaml.loader import SafeLoader
+
+from .device_messenger import DeviceMessenger
+from .metrics_dict import METRICS
 
 metrics = []
 
 
 def connect_mqtt(broker, port, device_name, metrics_list, period) -> mqtt_client:
-    thread_info = {'thread': None,
-                   'must_work': False}
+    thread_info = {"thread": None, "must_work": False}
 
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
-            d = DeviceMessenger(client=client, device_name=device_name)
-            create_device(metrics_list, d)
+            messenger = DeviceMessenger(client=client, device_name=device_name)
+            create_device(metrics_list, messenger)
 
-            thread_info['must_work'] = True
-            thread_info['thread'] = Thread(target=thread2_loop, args=(period, d, thread_info, ))
-            thread_info['thread'].start()
+            thread_info["must_work"] = True
+            thread_info["thread"] = Thread(
+                target=thread2_loop,
+                args=(
+                    period,
+                    messenger,
+                    thread_info,
+                ),
+            )
+            thread_info["thread"].start()
         else:
             print("Failed to connect, return code %d\n", rc)
 
     def on_disconnect(client, userdata, rc):
         if rc != 0:
             print("Unexpected disconnection.")
-        thread_info['must_work'] = False
-        thread_info['thread'].join()
+        thread_info["must_work"] = False
+        thread_info["thread"].join()
 
-    client_id = 'python-mqtt-wb-{0}'.format(random.randint(0, 255))
+    client_id = "python-mqtt-wb-{0}".format(random.randint(0, 255))
     url = urllib.parse.urlparse(broker)
-    if url.scheme == 'unix':
+    if url.scheme == "unix":
         client = paho_socket.Client(client_id)
         client.on_connect = on_connect
         client.on_disconnect = on_disconnect
@@ -53,40 +60,43 @@ def connect_mqtt(broker, port, device_name, metrics_list, period) -> mqtt_client
     return client
 
 
-def create_device(metrics_list, d):
+def create_device(metrics_list, messenger):
     for metric in metrics_list:
-        metrics.append(METRICS[metric](d))
+        metrics.append(METRICS[metric](messenger))
 
 
-def thread2_loop(period, device_messenger: DeviceMessenger, thread_info):
-    while thread_info['must_work']:
+def thread2_loop(period, messenger: DeviceMessenger, thread_info):
+    while thread_info["must_work"]:
         for metric in metrics:
-            metric.send(device_messenger)
+            metric.send(messenger)
         time.sleep(period)
 
 
-def main(argv=sys.argv):
-    parser = argparse.ArgumentParser(description='The tool to send metrics')
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
 
-    parser.add_argument('-c', '--config', action='store', help='get data from config')
+    parser = argparse.ArgumentParser(description="The tool to send metrics")
+
+    parser.add_argument("-c", "--config", action="store", help="get data from config")
 
     args = parser.parse_args(argv[1:])
 
-    with open(args.config) as f:
+    with open(args.config, encoding="utf-8") as f:
         data = yaml.load(f, Loader=SafeLoader)
-        broker = data['mqtt']['broker']
+        broker = data["mqtt"]["broker"]
         try:
-            port = data['mqtt']['port']
+            port = data["mqtt"]["port"]
         except KeyError:
             port = 0
-        period = data['mqtt']['period']
-        device_name = data['mqtt']['device-name']
+        period = data["mqtt"]["period"]
+        device_name = data["mqtt"]["device-name"]
 
-        metrics_list = data['metrics']['list']
+        metrics_list = data["metrics"]["list"]
 
     client = connect_mqtt(broker, port, device_name, metrics_list, period)
     client.loop_forever()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
