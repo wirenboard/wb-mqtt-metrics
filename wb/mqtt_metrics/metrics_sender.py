ec72c4ab75117e6b139408cc80590b43f359a9b3
Nikita Maslov <nikita.maslov@wirenboard.ru>
2023-02-08 20:49:46 +0600

    do not use additional thread to publish values

--- wb/mqtt_metrics/metrics_sender.py
+++ wb/mqtt_metrics/metrics_sender.py
@@ -1,10 +1,7 @@
 import argparse
-
 import random
 import sys
 import time
-import urllib.parse
-from threading import Thread
 
 import yaml
 from paho.mqtt import client as mqtt_client
@@ -13,55 +10,25 @@ from yaml.loader import SafeLoader
 from .device_messenger import DeviceMessenger
 from .metrics_dict import METRICS
 
-metrics = []
 
-
-def connect_mqtt(broker, port, device_name, metrics_list, period) -> mqtt_client:
-    thread_info = {"thread": None, "must_work": False}
-
+def connect_mqtt(broker, port) -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
-            messenger = DeviceMessenger(client=client, device_name=device_name)
-            create_device(metrics_list, messenger)
-
-            thread_info["must_work"] = True
-            thread_info["thread"] = Thread(
-                target=thread2_loop,
-                args=(
-                    period,
-                    messenger,
-                    thread_info,
-                ),
-            )
-            thread_info["thread"].start()
         else:
             print("Failed to connect, return code %d\n", rc)
 
     def on_disconnect(client, userdata, rc):
         if rc != 0:
             print("Unexpected disconnection.")
-        thread_info["must_work"] = False
-        thread_info["thread"].join()
 
     client_id = random.randint(0, 255)
     client = mqtt_client.Client('python-mqtt-wb-{0}'.format(client_id))
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.connect(broker, port)
+
     return client
-
-
-def create_device(metrics_list, messenger):
-    for metric in metrics_list:
-        metrics.append(METRICS[metric](messenger))
-
-
-def thread2_loop(period, messenger: DeviceMessenger, thread_info):
-    while thread_info["must_work"]:
-        for metric in metrics:
-            metric.send(messenger)
-        time.sleep(period)
 
 
 def main(argv=None):
@@ -83,8 +50,22 @@ def main(argv=None):
 
         metrics_list = data["metrics"]["list"]
 
-    client = connect_mqtt(broker, port, device_name, metrics_list, period)
-    client.loop_forever()
+    client = connect_mqtt(broker, port)
+    messenger = DeviceMessenger(client=client, device_name=device_name)
+
+    metrics = []
+    for metric in metrics_list:
+        metrics.append(METRICS[metric](messenger))
+
+    try:
+        client.loop_start()
+
+        while True:
+            for metric in metrics:
+                metric.send(messenger)
+            time.sleep(period)
+    finally:
+        client.loop_stop()
 
 
 if __name__ == "__main__":
